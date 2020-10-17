f27cd3aab536f0eeef243423423f6c7642e2b61e
Veronica De Leon <v.dleon@gmail.com>
2020-10-17 15:10:17 +0200

    api connection

--- wolfram-alpha-app/src/App.js
+++ wolfram-alpha-app/src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import { Route, Switch } from "react-router-dom";
 import MoonPath from "./components/MoonPath"
+import SunPath from "./components/SunPath"
 
 function App() {
   return (
     <div className="App">
       <h1>Wolfram Alpha App</h1>
-      >
+      <Switch>
+      <Route exact path="/moon-path" component={MoonPath} />
+      <Route exact path="/sun-path" component={SunPath} /> 
+      </Switch>
     </div>
   );
 }
--- wolfram-alpha-app/src/components/SunPath.js
+++ wolfram-alpha-app/src/components/SunPath.js
@@ -1,19 +1,38 @@
+import React, { Component } from 'react'
 import axios from 'axios'
-import React from 'react'
 
-export default function SunPath() {
+export default class SunPath extends Component {
 
     state = {
-    sunPath: []
+     sunPath: []
+    };
+
+    getSunPathData = () => {
+    axios
+    .get("http://api.wolframalpha.com/v2/query?appid=6TP376-VPK96VK7R8&input=sun&output=json&location=Caracas")
+    .then((sunPathList) => {
+        //this.setState({ sunPath: sunPathList.data})
+        console.log( "this is the data", sunPathList.data.queryresult)
+    })
+    .catch(error => console.log(error))
+}
+
+    componentDidMount() {
+        this.getSunPathData()
     }
 
-getSunPathData = () => {
-    axios.get("http://api.wolframalpha.com/v2/query?6TP376-VPK96VK7R8=DEMO&input=test")
+    render() {
+        return (
+            <div>
+                <h2>Moon in Caracas</h2>
+                <h2>Moon in Santiago de Cuba</h2>
+                <h2>Moon in Serrekunda</h2>
+                <h2>Moon in Fukuoka</h2>
+            </div>
+        )
+    }
+    
 }
 
-    return (
-        <div>
-            
-        </div>
-    )
-}
+
+
